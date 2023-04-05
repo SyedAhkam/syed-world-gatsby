bbedc24e52cb88dc7e2d68190005bc71f9b76697
Syed Ahkam <smahkam57@gmail.com>
2023-04-06 02:00:03 +0530

    Setup chakra UI

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -37,6 +37,26 @@ const config: GatsbyConfig = {
       },
       __key: "pages",
     },
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+        /**
+         * @property {boolean} [isBaseProvider=false]
+         * if true, will render `<ChakraBaseProvider>`
+         */
+        isBaseProvider: false,
+      },
+    },
   ],
 };
 
